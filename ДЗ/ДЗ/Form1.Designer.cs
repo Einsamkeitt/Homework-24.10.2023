7b0f5c3be82f5f3586c865b3edcddfc763f2304b
Kristina Kiseleva <toma.toma.tomat1061@gmail.com>
2023-10-29 23:16:02 +0300

    Homework 24.10.2023
    Homework 24.10.2023
    ISP 313
    Kristina Kiseleva

---
+++ ДЗ/ДЗ/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ДЗ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logButton = new System.Windows.Forms.Button();
+            this.callTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.callLogListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // logButton
+            // 
+            this.logButton.Location = new System.Drawing.Point(472, 313);
+            this.logButton.Name = "logButton";
+            this.logButton.Size = new System.Drawing.Size(155, 28);
+            this.logButton.TabIndex = 0;
+            this.logButton.Text = "Выполнить";
+            this.logButton.UseVisualStyleBackColor = true;
+            this.logButton.Click += new System.EventHandler(this.logButton_Click);
+            // 
+            // callTypeComboBox
+            // 
+            this.callTypeComboBox.FormattingEnabled = true;
+            this.callTypeComboBox.Location = new System.Drawing.Point(415, 12);
+            this.callTypeComboBox.Name = "callTypeComboBox";
+            this.callTypeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.callTypeComboBox.TabIndex = 1;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(405, 230);
+            this.phoneNumberTextBox.Multiline = true;
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(222, 42);
+            this.phoneNumberTextBox.TabIndex = 2;
+            // 
+            // callLogListBox
+            // 
+            this.callLogListBox.FormattingEnabled = true;
+            this.callLogListBox.Location = new System.Drawing.Point(12, 12);
+            this.callLogListBox.Name = "callLogListBox";
+            this.callLogListBox.Size = new System.Drawing.Size(387, 329);
+            this.callLogListBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 356);
+            this.Controls.Add(this.callLogListBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.callTypeComboBox);
+            this.Controls.Add(this.logButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button logButton;
+        private System.Windows.Forms.ComboBox callTypeComboBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.ListBox callLogListBox;
+    }
+}
+
---
+++ ДЗ/ДЗ/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ДЗ
+{
+    public partial class Form1 : Form
+    {
+        public struct Call
+        {
+            public string Type;
+            public string PhoneNumber;
+            public DateTime Time;
+        }
+
+        private List<Call> callLog = new List<Call>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            callTypeComboBox.Items.Add("Входящий");
+            callTypeComboBox.Items.Add("Исходящий");
+            callTypeComboBox.Items.Add("Непринятый");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logButton_Click(object sender, EventArgs e)
+        {
+            
+            string callType = callTypeComboBox.Text;
+            string phoneNumber = phoneNumberTextBox.Text;
+            DateTime callTime = DateTime.Now;
+
+            
+            Call newCall = new Call
+            {
+                Type = callType,
+                PhoneNumber = phoneNumber,
+                Time = callTime
+            };
+
+            
+            callLog.Add(newCall);
+
+            
+            UpdateCallLog();
+        }
+
+        private void UpdateCallLog()
+        {
+            
+            callLogListBox.Items.Clear();
+
+            
+            foreach (var call in callLog)
+            {
+                callLogListBox.Items.Add($"{call.Type} звонок от {call.PhoneNumber} от {call.Time}");
+            }
+        }
+    }
+}
